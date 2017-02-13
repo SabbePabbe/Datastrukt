d8ada5ad4eb8baf5516175481fccc567ba517781
Sabrina Samuelsson <sabrina.sabbe@hotmail.com>
2017-02-13 23:34:18 +0100

    fix class declaration for splay

--- src/SplayWithGet.java
+++ src/SplayWithGet.java
@@ -1,6 +1,9 @@
 /**
  * Created by jonatan gustafsson & sabrina samuelsson on 13/02/2017.
  */
-public class SplayWithGet {
+public class SplayWithGet<E extends Comparable<? super E>>
+                            extends BinarySearchTree<E>
+                            implements Iterable<E> {
+
     
 }
