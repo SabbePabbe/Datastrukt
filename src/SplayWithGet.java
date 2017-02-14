a9ba10c26af481f05c7be4400fbe52df82b2c2c1
JonatanGust <jongust@student.chalmers.se>
2017-02-14 10:17:22 +0100

    litengrej

--- src/SplayWithGet.java
+++ src/SplayWithGet.java
@@ -6,6 +6,9 @@ public class SplayWithGet<E extends Comparable<? super E>>
                             implements Iterable<E> {
 
     //Från labbinstruktionerna:
+    /*Ni behöver endast utföra balanseringarna vid sökning i trädet,
+    även om det normalt sker även vid insättning i och borttagning ur ett Splay-träd.*/
+    
     /*Koden för balanseringarna: zig, zag, zigzag och zagzig, kan ni kopiera från AVL-trädet i zip-filen och
     bara döpa om metoderna samt stryka allt som har med höjden att göra. Koden för balanseringarna: zagzag
     och zigzig, skall ni skriva själva. Observera att benämningarna zigzag, zigzig osv inte används på riktigt
