8028c66c8e0713caec936d4d1c8903ecd12b06da
JonatanGust <jongust@student.chalmers.se>
2017-02-21 11:29:04 +0100

    this is todo

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -5,18 +5,20 @@ public class DirectedGraph<E extends Edge> {
 
 
 	public DirectedGraph(int noOfNodes) {
-		;
+		//TODO make new pool of nodes
 	}
 
 	public void addEdge(E e) {
-		;
+		//TODO add edge
 	}
 
 	public Iterator<E> shortestPath(int from, int to) {
+		//TODO Dikstra cycle through connected nodes and build ways, see wich one is shortest with returnval (just do recursive return?)
 		return null;
 	}
 		
 	public Iterator<E> minimumSpanningTree() {
+		//TODO use kruskal
 		return null;
 	}
 
