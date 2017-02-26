a192b3e6a97c57d2de4fa26a3873458407f6b8db
Sabrina Samuelsson <sabrina.sabbe@hotmail.com>
2017-02-26 13:31:33 +0100

    add pseudocode from slides

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -15,6 +15,37 @@ public class DirectedGraph<E extends Edge> {
 	public Iterator<E> shortestPath(int from, int to) {
 		//TODO Dikstra cycle through connected nodes and build ways, see wich one is shortest with returnval (just do recursive return?)
 		return null;
+
+		/*
+
+		pseudocode dijkstra(G: graph (V,E))
+			ssf : array(1..n) of shortest paths so far
+			v, w, u: nodes
+			S = {1} // the start node
+			while S ≠ V loop
+				choose the edge (u, w) with
+					minimum cost s.t. u∈S and w∈V-S
+				add (w) to S (and remove from V-S)
+				for all v on EL(w) loop
+					if it is shorter to go by way of v
+						update ssf(v) and p(v)
+			end loop
+		end dijkstra
+
+
+		//variant en till en
+		lägg (startnod, 0, tom väg) i en p-kö
+		while kön inte är tom
+			(nod, cost, path) = första elementet i p-kön
+			if nod ej är besökt
+				if nod är slutpunkt returnera path
+				else
+				markera nod besökt
+				for every v on EL(nod)
+					if v ej är besökt
+					lägg in nytt köelement
+						för v i p-kön
+		 */
 	}
 		
 	public Iterator<E> minimumSpanningTree() {
