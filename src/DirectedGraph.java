9e11863a0bb90ef7d52a451f7a81621dd8226be9
Sabrina Samuelsson <sabrina.sabbe@hotmail.com>
2017-03-02 15:56:49 +0100

    comments

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -8,6 +8,13 @@ public class DirectedGraph<E extends Edge> {
 	private ArrayList<LinkedList<E>> nodes;
 	private int noOfNodes;
 
+
+
+	/**
+	 * Intializes a graph with noOfNodes nodes and 0 edges.
+	 * Implementation uses a LinkedList using an ArrayList (don't use to big of a graph!)
+	 * @param noOfNodes
+	 */
 	public DirectedGraph(int noOfNodes) {
 		this.noOfNodes = noOfNodes;
 		nodes = new ArrayList<>(noOfNodes);
@@ -17,54 +24,72 @@ public class DirectedGraph<E extends Edge> {
 		}
 	}
 
+	/**
+	 * Adds the edge to the (list) start node (e.from) index in the LinkedList
+	 * @param e
+	 */
 	public void addEdge(E e) {
 		nodes.get(e.from).add(e);
 	}
 
+	/**
+	 * Finds the shortest path starting in from and ending in to using an implementation of the Dijkstra Algorithm.
+	 * @param from
+	 * @param to
+	 * @return The found path as an iterator (over an ArrayList)
+	 */
 	public Iterator<E> shortestPath(int from, int to) {
 
-		//lista över vilka noder som har blivit besökta
+		//keeps track of which nodes have been visited
 		boolean[] nodeVisited = new boolean[noOfNodes];
 
+		//initialize PQ with Comparator (CompDijkstraPath)
 		PriorityQueue<DijkstraQueueElement> dijkstraPQ = new PriorityQueue<DijkstraQueueElement>(new CompDijkstraPath());
 
-		//custom konstruktor för första elementet, pga de andra vill lägga till en edge i path
-		dijkstraPQ.add(new DijkstraQueueElement(from)); //lägg (startnod, 0, tom väg) i en p-kö
+		//custom constructor for first element
+		dijkstraPQ.add(new DijkstraQueueElement(from));
 
 		DijkstraQueueElement element;
 
 		while(dijkstraPQ.size() > 0){
 
-			element = dijkstraPQ.poll(); //(nod, cost, path) = första elementet i p-kön
+			element = dijkstraPQ.poll(); //(node, cost, path) = first element of pq
 			int node = element.getNode();
 			if(!nodeVisited[node]){
 				if(node == to){
-					//nodeVisited[node] = true;
+					//goal reached, return path
 					return element.getPath().iterator();
 				} else {
 					nodeVisited[node] = true;
 					//for every neighbour of our node:
 					for (E e:nodes.get(node)) {
-
 						if (!nodeVisited[e.to]){
-
+							//add a new dijkstra element to the pq
 							dijkstraPQ.add(new DijkstraQueueElement(e,
-									element.getCost() + e.getWeight(), 
+									element.getCost() + e.getWeight(),
 									element.getPath()));
 						}
 					}
 				}
 			}
 		}
-		return null;
+		return null; //could not find a path, graph is not a connected graph
 	}
 
-
+	/**
+	 * Our wrapper element containing a node, the cost of the traversed path so far, and the path itself
+	 */
 	private class DijkstraQueueElement {
 		private int node;
 		private double cost;
 		private ArrayList<E> path;
 
+		/**
+		 * Standard constructor
+		 * @param edge The edge we used to get here
+		 * @param cost The cost of the total path so far (including that of edge)
+		 * @param path The path so far (not including edge)
+		 */
 		public DijkstraQueueElement(E edge, double cost, ArrayList<E> path){
 			this.node = edge.to;
 			this.cost = cost;
@@ -72,7 +97,10 @@ public class DirectedGraph<E extends Edge> {
 			this.path.add(edge);
 		}
 
-		//custom konstruktor för första elementet
+		/**
+		 * Custom constructor for first element, does not have cost or path yet
+		 * @param node Start node
+		 */
 		public DijkstraQueueElement(int node){
 			this.node = node;
 			cost = 0;
@@ -92,6 +120,9 @@ public class DirectedGraph<E extends Edge> {
 		}
 	}
 
+	/**
+	 * We chose to implement a comparator for paths to be used in a priority queue for Dijkstra's algorithm.
+	 */
 	private class CompDijkstraPath implements Comparator<DijkstraQueueElement> {
 		@Override
 		public int compare(DijkstraQueueElement o1, DijkstraQueueElement o2) {
@@ -99,6 +130,9 @@ public class DirectedGraph<E extends Edge> {
 		}
 	}
 
+	/**
+	 * We chose to implement a comparator for edges to be used in a priority queue for Kruskal's algorithm.
+	 */
 	private class CompKruskalEdge implements Comparator<E>{
 		@Override
 		public int compare(E o1, E o2) {
@@ -108,7 +142,10 @@ public class DirectedGraph<E extends Edge> {
 	}
 
 
-
+	/**
+	 * Our implementation of a Kruskal MST
+	 * @return Iterator over a LinkedList containing all edges in the MST
+	 */
 	public Iterator<E> minimumSpanningTree() {
 
 		if (noOfNodes == 0){
@@ -117,24 +154,28 @@ public class DirectedGraph<E extends Edge> {
 
 		ArrayList<LinkedList<E>> cc = new ArrayList<>(noOfNodes);
 
+		//initialize the list of lists
 		for(int i = 0; i < noOfNodes; i++){
 			cc.add(new LinkedList<>());
 		}
 
 		PriorityQueue<E> pq = new PriorityQueue<>(new CompKruskalEdge());
 
+		//add all edges to the priority queue
 		for (LinkedList<E> ll : nodes){
 			pq.addAll(ll);
 		}
 
 		int noOfListsLeft = noOfNodes;
 
+		//while there are edges left and the MST is incomplete
 		while (!pq.isEmpty() && (noOfListsLeft > 1)){
 			E e = pq.poll();
 
+			//if not same list
 			if (!(cc.get(e.from) == cc.get(e.to))){
-
 				if((cc.get(e.from).size() > cc.get(e.to).size())){
+					//move every edge from smaller to bigger list
 					for(E edge : cc.get(e.to) ) {
 						cc.get(e.from).add(edge);
 						//iterate over edges and move pointers
@@ -144,6 +185,7 @@ public class DirectedGraph<E extends Edge> {
 					cc.set(e.to,cc.get(e.from));
 
 				} else {
+					//same as above except for when the other list is bigger (or they are same size)
 					for(E edge : cc.get(e.from) ) {
 						cc.get(e.to).add(edge);
 						//iterate over edges and move pointers
@@ -154,7 +196,9 @@ public class DirectedGraph<E extends Edge> {
 					cc.set(e.from,cc.get(e.to));
 
 				}
+				//decrease counter of lists (unconnected nodes)
 				noOfListsLeft--;
+				//add current edge to the list
 				cc.get(e.to).add(e);
 			}
 		}
